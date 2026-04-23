d8cf100c01dca420aa77e85a0af08b1ca5c5ef40
CristianBlanco30 <blancos341@gmail.com>
2026-04-23 15:43:38 +0200

    Add functions for sum and difference calculations

---
+++ Code/08_functions.R
@@ -0,0 +1,14 @@
+# My functions!
+
+somma <- function(x,y) {
+  z=x+y
+  return(z)
+}
+somma(1,100)
+
+# excercise: make a function called difference
+
+differenza <- function(x,y) {
+  z=x-y
+  return(z)
+}
